344429fc4b41ef48ced0538312a06ce6b8baa3cf
root <root@localhost.localdomain>
2019-10-03 23:40:43 +0530

    home page is working using styled components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,101 @@
 import React, { Component } from 'react'
 import styles from './css/home.module.css'
 import { Link } from 'react-router-dom';
-
+import styled from 'styled-components';
 import temple from './res/temple1.jpg';
+
+const Wrapper = styled.div`
+    margin: 0%;
+    padding: 0%;
+    height: 100vh;
+`;
+
+const Header = styled.div`
+    background-color: rgba(0, 0, 0, 0.541);
+    padding-top: 0%;
+    margin-top: 0%;
+    position: absolute;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+
+`;
+
+const Overlay = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-image: linear-gradient(to bottom right, #d442f5b7, #410082b7);
+    position: absolute;
+    top: 0%;
+
+    @media (max-width: 1000px) {
+        height: 130vh;
+        width: 100%;
+        background-image: linear-gradient(to bottom right, #d442f5b7, #410082b7);
+        position: absolute;
+        top: 0%;
+    }
+    
+    @media (max-width: 550px) {
+        height: 210vh;
+        width: 100%;
+        background-image: linear-gradient(to bottom right, #d442f5b7, #410082b7);
+        position: absolute;
+        top: 0%;
+    }
+`;
+
+const Section = styled.div`
+    position: relative;
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    max-height: 90vh;
+    max-width: 100vw;
+    padding-top: 150px;
+`;
+
+const Container = styled.div`
+    display: grid;
+    grid-template-areas: "a a a";
+    grid-gap: 50px 150px;
+    grid-auto-rows: auto;
+
+    @media (max-width: 1000px) {
+        display: grid;
+        grid-template-areas: "a a";
+        grid-gap: 50px 150px;
+        grid-auto-rows: auto;
+    }
+    @media (max-width: 550px) {  
+        display: grid;
+        grid-template-areas: "a";
+        grid-gap: 50px 150px;
+        grid-auto-rows: auto;
+    }
+
+`;
 
 class Home extends Component {
     render() {
         return (
 
-            <div >
+            <Wrapper>
                 <img src={temple} alt="" />
-                <div className={styles.overlay}>
-                    <header>
-                        <Link to="/" className={styles.logo_title}>
-                            {/* <a href="index.html" className="logo-title"> */}
+                <Overlay>
+                    <Header>
+                        <Link to="/home" className={styles.logo_title}>
                             <h1>Logo</h1>
-                            {/* </a> */}
                         </Link>
                         <Link to='/home' className={styles.user}>
-                            <i className="fas fa-user-circle fa-2x" />
+                            <i className="fas fa-user-circle fa-2x" />Account
                         </Link>
-                        {/* <a href="Home.html" className="user"><i className="fas fa-user-circle fa-2x"></i></a> */}
-                    </header>
-                    <section>
-                        <div className={styles.container}>
 
-                            <Link to='/home/audio'>
+                    </Header>
+                    <Section>
+                        <Container>
+
+                            <Link to='/home/audio' className={styles.A}>
                                 <div className={styles.box_1 + ' ' + styles.boxstyle}>
                                     <div className={styles.content}>
                                         <i className="fas fa-music fa-3x"></i>
@@ -34,7 +104,7 @@ class Home extends Component {
                                 </div>
                             </Link>
 
-                            <Link to='/home/video'>
+                            <Link to='/home/video' className={styles.A}>
                                 <div className={styles.box_2 + ' ' + styles.boxstyle}>
                                     <div className={styles.content}>
                                         <i className="fas fa-film fa-3x"></i>
@@ -42,7 +112,7 @@ class Home extends Component {
                                     </div>
                                 </div>
                             </Link>
-                            <Link to='/home/ebooks'>
+                            <Link to='/home/ebooks' className={styles.A}>
                                 <div className={styles.box_3 + ' ' + styles.boxstyle}>
                                     <div className={styles.content}>
                                         <i className="fas fa-atlas fa-3x"></i>
@@ -51,7 +121,7 @@ class Home extends Component {
                                 </div>
                             </Link>
 
-                            <Link to='/home/training'>
+                            <Link to='/home/training' className={styles.A}>
                                 <div className={styles.box_4 + ' ' + styles.boxstyle}>
                                     <div className={styles.content}>
                                         <i className="fas fa-user-graduate fa-3x"></i>
@@ -60,7 +130,7 @@ class Home extends Component {
                                 </div>
                             </Link>
 
-                            <Link to='/home/dictionary'>
+                            <Link to='/home/dictionary' className={styles.A}>
                                 <div className={styles.box_5 + ' ' + styles.boxstyle}>
                                     <div className={styles.content}>
                                         <i className="fas fa-spell-check fa-3x"></i>
@@ -68,12 +138,11 @@ class Home extends Component {
                                     </div>
                                 </div>
                             </Link>
+                        </Container>
+                    </Section>
+                </Overlay>
 
-                        </div>
-                    </section>
-                </div>
-
-            </div >
+            </Wrapper >
         )
     }
 }
